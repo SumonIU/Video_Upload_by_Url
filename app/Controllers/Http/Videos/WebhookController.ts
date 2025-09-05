92ad7c20f4ff0b1e3f589b24427a3763be3fc1eb
sumonIU <bilbobaggins0246>
2025-09-05 19:12:32 +0600

    webhook better visualization

--- app/Controllers/Http/Videos/WebhookController.ts
+++ app/Controllers/Http/Videos/WebhookController.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk"
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class WebhookController {
      public async webhook({ request, response }: HttpContextContract){
@@ -5,11 +6,25 @@ export default class WebhookController {
     console.log(request.all());
 
     // You can handle different statuses here
-    const { VideoLibraryId, VideoGuid, Status } = request.all();
-    if (Status === 3) {
-        console.log(`Video ${VideoGuid} finished processing!`);
+    const { Status } = request.all();
+    if(Status=== 0){
+        console.log(`video is queued for encoding`)
     }
-
+    else if(Status===1){
+        console.log(`video is processing`)
+    }
+    else if(Status===2){
+        console.log(`video is encoding`)
+    }
+    else if(Status===3){
+        console.log(`video encoding has finished`)
+    }
+    else if(Status===4){
+        console.log(chalk.green(`video Resolution finished`))
+    }
+    else if(Status===5){
+        console.log(chalk.red("video uploading has Failed!"))
+    }
     // Respond 200 OK to Bunny.net
     return response.status(200).send('OK');
 }
